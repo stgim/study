00cd860b3c7e506e693e38e5db01698cc9642fbb
stgim875 <stgim875@gmail.com>
2020-10-27 00:37:55 +0900

    2020.10.27 업데이트

--- 12.반복문/4. 활용/4.rb
+++ 12.반복문/4. 활용/4.rb
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # i = 0
 # while i < 10:
 #     print('print("Hell world '+i+'")')
 #     i = i + 1
+# end
 
 i = 0
-while i < 10:
-    print('print("Hell world '+str(i*9)+'")')
+while i < 10
+    puts('print("Hell world '+(i*9).to_s()+'")')
     i = i + 1
-    
+end
